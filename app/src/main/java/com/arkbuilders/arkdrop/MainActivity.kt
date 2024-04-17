13c811c0b0d1833a8458be548b92b1dc1bcdcd7a
Hieu Vu <star.hieu.99@gmail.com>
2024-04-17 21:46:39 +0700

    Ask for permission

--- app/src/main/java/com/arkbuilders/arkdrop/MainActivity.kt
+++ app/src/main/java/com/arkbuilders/arkdrop/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.arkbuilders.arkdrop
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.Manifest.permission.READ_MEDIA_VIDEO
+import android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,6 +28,31 @@ import com.arkbuilders.arkdrop.ui.theme.ARKDropTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Register ActivityResult handler
+        val requestPermissions =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+                // Handle permission requests results
+                // See the permission example in the Android platform samples: https://github.com/android/platform-samples
+            }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            requestPermissions.launch(
+                arrayOf(
+                    READ_MEDIA_IMAGES,
+                    READ_MEDIA_VIDEO,
+                    READ_MEDIA_VISUAL_USER_SELECTED,
+                    CAMERA,
+                )
+            )
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions.launch(
+                arrayOf(
+                    READ_MEDIA_IMAGES,
+                    READ_MEDIA_VIDEO,
+                    CAMERA,
+                )
+            )
+        }
         setContent {
             ARKDropTheme {
                 // A surface container using the 'background' color from the theme

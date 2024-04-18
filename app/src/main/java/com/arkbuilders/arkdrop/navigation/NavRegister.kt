81002dafb8f66432c31fd0bb27e744b73fdbea27
Hieu Vu <star.hieu.99@gmail.com>
2024-04-18 16:25:33 +0700

    Add transfer confirmation screen

--- app/src/main/java/com/arkbuilders/arkdrop/navigation/NavRegister.kt
+++ app/src/main/java/com/arkbuilders/arkdrop/navigation/NavRegister.kt
@@ -5,15 +5,19 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.arkbuilders.arkdrop.Greeting
 import com.arkbuilders.arkdrop.presentation.filestransfers.FilesTransferScreen
+import com.arkbuilders.arkdrop.presentation.transferconfirmation.TransferConfirmation
 
 fun NavGraphBuilder.navRegistration(navController: NavController) {
     composable(TransfersDestination.route) {
-        FilesTransferScreen()
+        FilesTransferScreen(navController = navController)
     }
     composable(HistoryDestination.route) {
         Greeting(name = "HistoryDestination")
     }
     composable(SettingsDestination.route) {
         Greeting(name = "SettingsDestination")
+    }
+    composable(TransferConfirmationDestination.route) {
+        TransferConfirmation(navController = navController)
     }
 }

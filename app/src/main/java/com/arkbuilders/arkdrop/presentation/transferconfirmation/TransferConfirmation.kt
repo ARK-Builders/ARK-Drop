83582798106e3e5b846c6f6044c4883613d23cc8
Hieu Vu <star.hieu.99@gmail.com>
2024-04-19 20:36:39 +0700

    Ui tweak with scaffold

--- app/src/main/java/com/arkbuilders/arkdrop/presentation/transferconfirmation/TransferConfirmation.kt
+++ app/src/main/java/com/arkbuilders/arkdrop/presentation/transferconfirmation/TransferConfirmation.kt
@@ -23,10 +23,14 @@ import androidx.compose.material.icons.outlined.QrCodeScanner
 import androidx.compose.material.icons.outlined.Visibility
 import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -47,130 +51,146 @@ import com.arkbuilders.arkdrop.R
 import com.arkbuilders.arkdrop.presentation.qrcodescanner.QRCodeScannerActivity
 import com.arkbuilders.arkdrop.ui.theme.BlueDark600
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TransferConfirmation(
     modifier: Modifier = Modifier,
     navController: NavController
 ) {
-
-    val isConfirmationCodeShown = remember {
-        mutableStateOf(false)
-    }
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color(0xFFFCFCFD))
-            .padding(horizontal = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(
-            modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            TextButton(
-                onClick = {
-                    navController.navigateUp()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.White
+                ),
+                title = {},
+                navigationIcon = {
+                    TextButton(
+                        onClick = {
+                            navController.navigateUp()
+                        },
+                        colors = ButtonDefaults.textButtonColors(
+                            contentColor = Color.Black
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBackIosNew,
+                            contentDescription = null
+                        )
+                        Spacer(modifier = modifier.width(ButtonDefaults.IconSpacing))
+                        Text("Back")
+                    }
                 },
-                colors = ButtonDefaults.textButtonColors(
-                    contentColor = Color.Black
-                )
+                actions = {
+                    val context = LocalContext.current
+                    TextButton(
+                        onClick = {
+                            // Launch camera for QR scanning
+                            context.startActivity(
+                                Intent(
+                                    context,
+                                    QRCodeScannerActivity::class.java
+                                )
+                            )
+                        },
+                        colors = ButtonDefaults.textButtonColors(
+                            contentColor = BlueDark600
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.QrCodeScanner,
+                            contentDescription = null
+                        )
+                        Spacer(modifier = modifier.width(ButtonDefaults.IconSpacing))
+                        Text("Scan")
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        val isConfirmationCodeShown = remember {
+            mutableStateOf(false)
+        }
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(Color(0xFFFCFCFD))
+                .padding(padding)
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(
-                    imageVector = Icons.Filled.ArrowBackIosNew,
-                    contentDescription = null
-                )
-                Spacer(modifier = modifier.width(ButtonDefaults.IconSpacing))
-                Text("Back")
+                Icon(imageVector = Icons.Outlined.Lock, contentDescription = null)
+                Spacer(modifier = modifier.width(12.dp))
+                Text("Confirmation code", style = MaterialTheme.typography.bodyLarge)
             }
-            val context = LocalContext.current
+            Spacer(modifier = modifier.height(12.dp))
+            Text(
+                modifier = modifier
+                    .clip(RoundedCornerShape(100 / 2))
+                    .background(Color.Gray.copy(0.2f))
+                    .padding(12.dp),
+                text = maskedText(isConfirmationCodeShown.value),
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = modifier.height(8.dp))
             TextButton(
                 onClick = {
-                    // Launch camera for QR scanning
-//                    launcher.launch(Uri.parse("image/*"))
-                    context.startActivity(Intent(context, QRCodeScannerActivity::class.java))
+                    isConfirmationCodeShown.value = !isConfirmationCodeShown.value
                 },
                 colors = ButtonDefaults.textButtonColors(
                     contentColor = BlueDark600
                 )
             ) {
                 Icon(
-                    imageVector = Icons.Outlined.QrCodeScanner,
+                    imageVector = extractIcon(isConfirmationCodeShown.value),
                     contentDescription = null
                 )
                 Spacer(modifier = modifier.width(ButtonDefaults.IconSpacing))
-                Text("Scan")
+                Text(extractText(isConfirmationCodeShown.value))
             }
-        }
-        Row(
-            modifier = modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(imageVector = Icons.Outlined.Lock, contentDescription = null)
-            Spacer(modifier = modifier.width(12.dp))
-            Text("Confirmation code", style = MaterialTheme.typography.bodyLarge)
-        }
-        Spacer(modifier = modifier.height(12.dp))
-        Text(
-            modifier = modifier
-                .clip(RoundedCornerShape(100 / 2))
-                .background(Color.Gray.copy(0.2f))
-                .padding(12.dp),
-            text = maskedText(isConfirmationCodeShown.value),
-            style = MaterialTheme.typography.titleMedium,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(modifier = modifier.height(8.dp))
-        TextButton(
-            onClick = {
-                isConfirmationCodeShown.value = !isConfirmationCodeShown.value
-            },
-            colors = ButtonDefaults.textButtonColors(
-                contentColor = BlueDark600
-            )
-        ) {
-            Icon(
-                imageVector = extractIcon(isConfirmationCodeShown.value),
+            Spacer(modifier = modifier.height(64.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                modifier = modifier.size(320.dp),
                 contentDescription = null
             )
-            Spacer(modifier = modifier.width(ButtonDefaults.IconSpacing))
-            Text(extractText(isConfirmationCodeShown.value))
-        }
-        Spacer(modifier = modifier.height(64.dp))
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            modifier = modifier.size(320.dp),
-            contentDescription = null
-        )
-        Spacer(modifier = modifier.height(24.dp))
-        Text(
-            modifier = modifier.fillMaxWidth(),
-            text = "Waiting for connect...",
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.titleMedium
-        )
-        Spacer(modifier = modifier.height(12.dp))
-        Row(
-            modifier = modifier
-                .border(
-                    width = 1.dp, color = Color.LightGray,
-                    shape = RoundedCornerShape(4.dp)
-                )
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Icon(
-                imageVector = Icons.Outlined.Link, contentDescription = null,
-                modifier = modifier.size(24.dp)
-            )
-            Text("Hash Code:", modifier = modifier.padding(horizontal = 6.dp))
+            Spacer(modifier = modifier.height(24.dp))
             Text(
-                "3910-LKA9-28HS-HAXX-72LA",
-                fontWeight = FontWeight.SemiBold
+                modifier = modifier.fillMaxWidth(),
+                text = "Waiting for connect...",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium
             )
+            Spacer(modifier = modifier.height(12.dp))
+            Row(
+                modifier = modifier
+                    .border(
+                        width = 1.dp, color = Color.LightGray,
+                        shape = RoundedCornerShape(4.dp)
+                    )
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Link, contentDescription = null,
+                    modifier = modifier.size(24.dp)
+                )
+                Text("Hash Code:", modifier = modifier.padding(horizontal = 6.dp))
+                Text(
+                    "3910-LKA9-28HS-HAXX-72LA",
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
         }
     }
 }

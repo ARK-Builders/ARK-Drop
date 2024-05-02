44c6d56644fef3122ad13bd68a574c072372cee3
Hieu Vu <star.hieu.99@gmail.com>
2024-05-02 19:47:35 +0700

    Extract string resource

--- app/src/main/java/com/arkbuilders/arkdrop/presentation/feature/editprofile/EditProfileScreen.kt
+++ app/src/main/java/com/arkbuilders/arkdrop/presentation/feature/editprofile/EditProfileScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.arkbuilders.arkdrop.R
@@ -47,7 +48,7 @@ fun EditProfileScreen(modifier: Modifier = Modifier) {
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = Color.White
                 ),
-                title = { Text("Edit Profile") },
+                title = { Text(stringResource(R.string.edit_profile_title)) },
                 navigationIcon = {
                     IconButton(
                         onClick = {

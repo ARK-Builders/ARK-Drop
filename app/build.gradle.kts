df09f7079dbb458c81dc89fb83d327de5ff1918d
Hieu Vu <star.hieu.99@gmail.com>
2024-05-05 14:59:44 +0700

    UI flow for sending files (#1)

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "dev.arkbuilders.arkdrop"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "dev.arkbuilders.arkdrop"
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation (libs.androidx.camera.mlkit.vision)
+    implementation(libs.barcode.scanning)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/dev/arkbuilders/arkdrop/presentation/feature/transferprogress/TransferProgressScreen.kt
@@ -0,0 +1,143 @@
+package dev.arkbuilders.arkdrop.presentation.feature.transferprogress
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.outlined.AddCircleOutline
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import dev.arkbuilders.arkdrop.presentation.feature.transferprogress.composables.FileItem
+import dev.arkbuilders.arkdrop.presentation.feature.transferprogress.composables.FileTransferAlertDialog
+import dev.arkbuilders.arkdrop.presentation.feature.transferprogress.composables.TransferParticipantHeader
+import dev.arkbuilders.arkdrop.ui.theme.Background
+import dev.arkbuilders.arkdrop.ui.theme.BlueDark600
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TransferProgressScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    val openAlertDialog = remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.White
+                ),
+                title = { Text("Transferring Files") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.navigateUp()
+                        },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Close,
+                            contentDescription = null
+                        )
+                    }
+                })
+        }
+    ) { padding ->
+        Column(
+            modifier = modifier
+                .background(Background)
+                .fillMaxSize()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TransferParticipantHeader()
+            LazyColumn(
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                items(listOf(1, 2, 3)) { item ->
+                    Box(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .border(
+                                width = 0.5.dp, color = Color.LightGray,
+                                shape = RoundedCornerShape(25)
+                            )
+                            .padding(16.dp)
+                    ) {
+                        FileItem(modifier = modifier,
+                            onCloseIconClick = {
+                                openAlertDialog.value = true
+                            }
+                        )
+                    }
+                }
+            }
+            OutlinedButton(
+                onClick = {
+
+                },
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = BlueDark600
+                ),
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = BlueDark600,
+                )) {
+                Icon(
+                    imageVector = Icons.Outlined.AddCircleOutline,
+                    contentDescription = null
+                )
+                Text("Send more")
+            }
+
+            if (openAlertDialog.value) {
+                FileTransferAlertDialog(
+                    onDismissRequest = { openAlertDialog.value = false },
+                    onConfirmation = { /*TODO*/ },
+                    dialogTitle = "Cancel this file",
+                    dialogText = "When you remove this file it cannot be undone.",
+                ) {
+                    FileItem(modifier = modifier)
+                }
+            } else {
+
+            }
+        }
+    }
+
+
+}
+
+@Preview
+@Composable
+fun PreviewTransferProgressScreen() {
+    TransferProgressScreen(navController = rememberNavController())
+}

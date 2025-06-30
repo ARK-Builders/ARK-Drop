d37b1b4dc30773a18815fd6564d5a48350a2a691
oluiscabral <oluiscabral@hotmail.com>
2025-06-30 18:07:17 -0300

    PoC version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,56 +1,62 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "dev.arkbuilders.arkdrop"
-    compileSdk = 34
+    namespace = "dev.arkbuilders.drop.app"
+    compileSdk = 36
+
+    signingConfigs {
+        create("release") {
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            storeFile = file(System.getenv("KEYSTORE_PATH"))
+        }
+    }
 
     defaultConfig {
-        applicationId = "dev.arkbuilders.arkdrop"
-        minSdk = 31
-        targetSdk = 34
+        applicationId = "dev.arkbuilders.drop.app"
+        minSdk = 29
+        targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = System.getenv("RELEASE_VERSION") ?: "dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
-    }
+
     packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+        jniLibs.excludes.add("META-INF/AL2.0")
+        jniLibs.excludes.add("META-INF/LGPL2.1")
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,27 +65,48 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.material.icons.extended)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
 
+    // NAVIGATION
+    implementation(libs.androidx.navigation.compose)
+
+    // Bindings setup
+    implementation(libs.jna) {
+        artifact {
+            extension = "aar"
+            type = "aar"
+        }
+    }
+    //noinspection Aligned16KB
+    implementation("dev.arkbuilders:drop:1.1.4") {
+        artifact {
+            extension = "aar"
+            type = "aar"
+        }
+    }
+
+    // QR CODE create setup
+    implementation(libs.google.zxing.core)
+    implementation(libs.google.zxing.javase)
+    implementation(libs.github.yuriy.budiyev.code.scanner)
+
+    // QR CODE SCAN
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.view)
-    implementation (libs.androidx.camera.mlkit.vision)
-    implementation(libs.barcode.scanning)
+    implementation(libs.mlkit.barcode.scanning)
+    implementation(libs.accompanist.permissions)
 
+    // DEVELOPMENT SETUP
     testImplementation(libs.junit)
-
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+tasks.named<Delete>("clean") {
+    delete(fileTree("$projectDir/src/main/jniLibs"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 pluginManagement {
     repositories {
         google {
@@ -11,13 +13,31 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+val localProperties = Properties()
+if (rootProject.projectDir.resolve("local.properties").exists()) {
+    localProperties.load(rootProject.projectDir.resolve("local.properties").inputStream())
+}
+val githubToken = localProperties.getProperty("github.token") ?: System.getenv("GITHUB_TOKEN")
+?: throw IllegalStateException("GITHUB_TOKEN not found")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven {
+            name = "drop-core GitHub Packages"
+            url = uri("https://maven.pkg.github.com/oluiscabral/drop-core")
+            credentials {
+                username = "token"
+                password = githubToken
+            }
+        }
     }
+
 }
 
-rootProject.name = "ARK Drop"
+rootProject.name = "Drop"
 include(":app")
+ 
